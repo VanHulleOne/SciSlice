fdfc96c20b2aa605cbeec24cc19ca27e3eed4412
lvanhulle <lvanhulle@wisc.edu>
2015-10-23 12:14:58 -0500

    Added testMethod1

--- Python1.py
+++ Python1.py
@@ -487,6 +487,9 @@ with open(startEndSubDirectory + '\\' + end_Gcode_FileName) as endFile:
 f.close()
 print 'Done writing File.'  
 
+def testMethod1(instuff):
+    return instuff+1
+
 """
 for loop in fullPath:
     f.write('(Loop Number {:.0f})\n'.format(loopNumber))
