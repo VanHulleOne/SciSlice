6a7439136cdc4eb546e921dda18469cd88463aef
lvanhulle <lvanhulle@wisc.edu>
2015-10-28 14:22:41 -0500

    Removed methods from Python1, created Shape class

---
+++ Shape.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 14:09:55 2015
+
+@author: lvanhulle
+"""
+
+class Shape:
+    
+    def __init__(self):
+        self.lines = []
+        self.minX = None
+        self.minY = None
+        self.maxX = None
+        self.maxY = None
+        
+    def addLine(self, line):
+        self.lines.append(line)
+        if(line.upperLeft.getX() < self.minX): self.minX = line.upperLeft.getX()
+        if(line.upperLeft.getY() > self.maxY): self.maxY = line.upperLeft.getY()
+        if(line.lowerRight.getX() > self.maxX): self.maxX = line.lowerRight.getX()
+        if(line.lowerRight.getY() < self.minY): self.mainY = line.lowerRight.getY()
+    
--- Test1.py
+++ Test1.py
@@ -10,10 +10,3 @@ import numpy
 point1 = p.Point(0,0)
 point2 = p.Point(0,2)
 point3 = p.Point(2,0)
-
-def getArea(p1, p2, p3):
-    matrix = [p1.getNormalVector(), p2.getNormalVector(), p3.getNormalVector()]
-    matrix = numpy.rot90(matrix)
-    return abs(numpy.linalg.det(matrix))
-
-print getArea(point2, point1, point3)
