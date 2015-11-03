f6f9da33b5c17c70dd5ac2926e735c92dd11e897
lvanhulle <lvanhulle@wisc.edu>
2015-11-03 14:56:12 -0600

    More work getting InFill working

--- InFill.py
+++ InFill.py
@@ -8,20 +8,45 @@ Created on Thu Oct 29 15:45:02 2015
 import Line as l
 import Point as p
 import Shape as s
+from LineGroup import LineGroup as LG
 
-class InFill:
+class InFill(LG):
     
-    def __init__(self, shape, angle, spacing, design):
-        self.shape = shape
+    PARTIAL_ROW = 0
+    FULL_ROW = 1
+    FULL_FIELD = 2
+    TRIMMED_FIELD = 3
+    
+    
+    
+    def __init__(self, trimShape, angle, spacing, design, designType):
+        LG.__init__(self, None)        
+        self.trimShape = trimShape
         self.angle = angle
         self.spacing = spacing
+        
+        self.operations = {0 : self.extendDesign,
+                           1 : self.createField,
+                           2 : self.trimField}
+        
         if(design == None):
-            point1 = p.Point(shape.minX, 0)
-            point2 = p.Point(shape.maxX, 0)
+            point1 = p.Point(trimShape.minX, 0)
+            point2 = p.Point(trimShape.maxX, 0)
             line = l.Line(point1, point2)
-            self.design = s.Shape()
-            self.design.addLine(line)
+            self.design = LG.LineGroup(line)
+            self.designType = self.FULL_ROW
         else:
             self.design = design
         
+        for i in range(self.designType, self.TRIMMED_FIELD):
+            self.operations[i]();
         
+    def extendDesign(self):
+        return 1
+        
+    def createField(self):
+        return 1
+        
+    def trimField(self):
+        return 1
+    
--- Test1.py
+++ Test1.py
@@ -5,11 +5,20 @@ Created on Wed Oct 28 10:16:22 2015
 @author: lvanhulle
 """
 import Point as p
-import InFill as infill
+#import InFill as infill
+import Shape as s
+import Line as l
 
-point1 = p.Point(0,0)
-point2 = p.Point(0,2)
-point3 = p.Point(2,0)
+p1 = p.Point(0,0)
+p2 = p.Point(1,1)
+p3 = p.Point(5,0)
+
+line1 = l.Line(p1, p2)
+line2 = l.Line(p2, p3)
 
 
-i = infill.InFill(1, 1, 1, None)
+s1 = s.Shape(None)
+s1.addLine(line1)
+s1.addLine(line2)
+
+print s1
