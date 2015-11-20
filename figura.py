6a5b364194b17765acec0a28dcc647ec0e680f88
VanHulleOne <beardedone85@gmail.com>
2015-11-20 12:10:07 -0600

    Wrote More methods
    Some methods are not complete but will work for 90 degree orientation.

--- figura.py
+++ figura.py
@@ -6,24 +6,46 @@ Created on Thu Nov 19 16:45:00 2015
 """
 
 import gcode as gc
-import parameters as para
+import parameters as pr
 
 class Figura:
     
-    def __init__(self, layer, zStep, numLayers):
-        self.layers = [layer.translate(0, 0, zStep*currentLayer) for currentLayer in range(1, numLayers+1)]
+    def __init__(self, layer):
+        self.layers = [layer.translate(0, 0, pr.layerHeight*(currentLayer+1)) for currentLayer in range(pr.numLayers)]
         self.gcode = ''
+        self.setGcode()
     
-    def getgcode(self):
+    def setGcode(self):
+        self.gcode += gc.startGcode()
         layerNumber = 1
         for layer in self.layers:
-            self.gcode += ';Layer: ' + str(layerNumber)
+            lines = self.organizeLines(layer.getLines())
+            self.gcode += '\n\n;Layer: ' + str(layerNumber) + '\n'
             self.gcode += ';T' + str(layerNumber) + '\n'
             self.gcode += ';M6\n'
-            self.gcode += gc.rapidMove(layer.lines[0].start)
-            self.gcode += gc.firstApproach(layer.lines[0].start)
+            self.gcode += gc.rapidMove(lines[0].start, pr.INCLUDE_Z)
+            self.gcode += gc.firstApproach(lines[0].start)
             
+            totalExtrusion = 0
+            for line in lines:
+                totalExtrusion += line.length*pr.extrusionRate
+                self.gcode += gc.rapidMove(line.start, pr.OMIT_Z)
+                self.gcode += gc.feedMove(line.end, pr.OMIT_Z, totalExtrusion)
             
+            self.gcode += gc.retractLayer(totalExtrusion, lines[-1].end)
+            layerNumber += 1
+        self.gcode += gc.endGcode()
+                
+                
+#TODO: change method to actually work for more than just 90 degree orientation            
+    def organizeLines(self, lines):
+        flip = False
+        for line in lines:
+            if flip:
+                line.flip()
+            flip = not flip
+        return lines
+        
     
     def __str__(self):
         tempString = ''
@@ -32,5 +54,6 @@ class Figura:
             tempString += ';T' + str(layerNumber) + '\n'
             tempString += ';M6\n'
             tempString += str(layer)
+            layerNumber += 1
         return tempString
     
