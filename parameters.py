5dcf2a24efb2057e5f0a5b70deeb73076acb6869
VanHulleOne <beardedone85@gmail.com>
2015-11-20 12:11:43 -0600

    Created a class to hold all of the parameters
    This class will eventually hold all of the printing parameters.

--- parameters.py
+++ parameters.py
@@ -6,3 +6,15 @@ Created on Thu Nov 19 19:52:29 2015
 """
 
 extrusionRate = 0.0212 #mm of filament/mm of travel
+printSpeed = 2000 #mm/min head travel speed
+pathWidth = 0.500 #mm distance between centerline of paths
+layerHeight = 0.3 #mm height per layer
+numLayers = 3 #number of layers to make
+
+OMIT_Z = True
+INCLUDE_Z = False
+RAPID = 8000. #mm/min
+TRAVERSE_RETRACT = 0.5 #mm to retract when traversing longer distances
+MAX_EXTRUDE_SPEED = 100 #mm/min max speed to move filament
+Z_CLEARANCE = 10.0 #mm to move Z up
+APPROACH_FR = 2000 #mm/min aproach feedrate
