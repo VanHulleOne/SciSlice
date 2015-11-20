0f320b03f1c30e41bde4c95b73ec5859afaf4e6f
VanHulleOne <beardedone85@gmail.com>
2015-11-19 19:58:04 -0600

    Started creating the body of Figura

--- figura.py
+++ figura.py
@@ -5,9 +5,32 @@ Created on Thu Nov 19 16:45:00 2015
 @author: lvanhulle
 """
 
-class Figura():
+import gcode as gc
+import parameters as para
+
+class Figura:
     
     def __init__(self, layer, zStep, numLayers):
-        self.layers = [layer.translate(0, 0, zStep*currentLayer) for currentLayer in range(numLayers)]
+        self.layers = [layer.translate(0, 0, zStep*currentLayer) for currentLayer in range(1, numLayers+1)]
+        self.gcode = ''
     
+    def getgcode(self):
+        layerNumber = 1
+        for layer in self.layers:
+            self.gcode += ';Layer: ' + str(layerNumber)
+            self.gcode += ';T' + str(layerNumber) + '\n'
+            self.gcode += ';M6\n'
+            self.gcode += gc.rapidMove(layer.lines[0].start)
+            self.gcode += gc.firstApproach(layer.lines[0].start)
+            
+            
+    
+    def __str__(self):
+        tempString = ''
+        layerNumber = 1
+        for layer in self.layers:
+            tempString += ';T' + str(layerNumber) + '\n'
+            tempString += ';M6\n'
+            tempString += str(layer)
+        return tempString
     
