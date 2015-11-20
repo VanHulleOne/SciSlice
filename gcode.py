1502c824569444ac77c38255fe832c0332f4b72e
VanHulleOne <beardedone85@gmail.com>
2015-11-20 12:10:32 -0600

    moved all parameters to parameters.py

--- gcode.py
+++ gcode.py
@@ -5,40 +5,41 @@ Created on Fri Nov 06 10:30:44 2015
 @author: lvanhulle
 """
 
-    
-RAPID = 8000 #mm/min
-TRAVERSE_RETRACT = 0.5 #mm to retract when traversing longer distances
-MAX_EXTRUDE_SPEED = 100 #mm/min max speed to move filament
-Z_CLEARANCE = 10.0 #mm to move Z up
-APPROACH_FR = 2000 #mm/min aproach feedrate
+import parameters as pr
 
-def feedMove(endPoint, ommitZ, extrusionAmount, printSpeed):
+def feedMove(endPoint, ommitZ, extrudeTo):
     if ommitZ:
         return ('G01 X{:.3f} Y{:.3f} F{:.0f} E{:.3f}\n'.format(endPoint.x, endPoint.y,
-                printSpeed, extrusionAmount))
+                pr.printSpeed, extrudeTo))
     else:
         return ('G01 X{:.3f} Y{:.3f} Z{:.3f} F{:.0f} E{:.3f}\n'.format(endPoint.x, endPoint.y, endPoint.z,
-                printSpeed, extrusionAmount))
+                pr.printSpeed, extrudeTo))
 
 def rapidMove(endPoint, ommitZ):
     if ommitZ:
         return ('G00 X{:.3f} Y{:.3f} F{:.0f}\n'.format(endPoint.x, endPoint.y,
-                RAPID))
+                pr.RAPID))
     else:
         return ('G00 X{:.3f} Y{:.3f} Z{:.3f} F{:.3f}\n'.format(endPoint.x, endPoint.y, endPoint.z,
-                RAPID))
+                pr.RAPID))
                 
 def retractLayer(currentE, currentPoint):
-    tempString = 'G1 E{:.3f} F{:.0f}\n'.format(currentE-TRAVERSE_RETRACT, MAX_EXTRUDE_SPEED)
-    tempString += 'G00 Z{:.3f} F{:.0f}\n'.format(currentPoint.z+Z_CLEARANCE, RAPID)
+    tempString = 'G1 E{:.3f} F{:.0f}\n'.format(currentE-pr.TRAVERSE_RETRACT, pr.MAX_EXTRUDE_SPEED)
+    tempString += 'G00 Z{:.3f} F{:.0f}\n'.format(currentPoint.z+pr.Z_CLEARANCE, pr.RAPID)
     return tempString
     
 def approachLayer(lastE, startPoint):
-    tempString = 'G1 Z{:.3} F{:.0} E{:.3}\n'.format(startPoint.z+Z_CLEARANCE/2.0,
-                    APPROACH_FR, lastE-TRAVERSE_RETRACT*0.75)
-    tempString += 'G1 Z{:.3} F{:.0} E{:.3}\n'.format(startPoint.z,
-                    APPROACH_FR/2.0, lastE)
+    tempString = 'G1 Z{:.3f} F{:.0f} E{:.3f}\n'.format(startPoint.z+pr.Z_CLEARANCE/2.0,
+                    pr.APPROACH_FR, lastE-pr.TRAVERSE_RETRACT*0.75)
+    tempString += 'G1 Z{:.3f} F{:.0f} E{:.3f}\n'.format(startPoint.z,
+                    pr.APPROACH_FR/2.0, lastE)
     return tempString
 
 def firstApproach(startPoint):
-    return 'G1 Z{:.3} F{:.0}\n'.format(startPoint.z)
+    return 'G1 Z{:.3f} F{:.0f}\n'.format(startPoint.z, pr.APPROACH_FR)
+    
+def startGcode():
+    return 'Start Gcode Here\n\n'
+
+def endGcode():
+    return 'End Gcode Here\n\n'
