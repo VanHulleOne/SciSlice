25126e6f120cc920f9a6e14029bcd848696d6b8b
VanHulleOne <beardedone85@gmail.com>
2015-12-03 16:54:49 -0600

    Finally removed all methods from python1

--- Python1.py
+++ Python1.py
@@ -17,40 +17,16 @@ import figura as fg
 import parameters as pr
 import gcode as gc
 from parameters import constants as c
+import doneShapes as ds
 
-    
-dogBone = s.Shape(None)
-dogBone.addLinesFromCoordinateList([[82.5, 0], [82.5, 9.5], [49.642, 9.5]])
-arc = a.Arc(p.Point(49.642, 9.5), p.Point(28.5, 6.5), c.CW, p.Point(28.5, 82.5), 20)
-dogBone.addLineGroup(arc)
-dogBone.addLinesFromCoordinateList([[28.5, 6.5], [0, 6.5]])
-dogBone.addLineGroup(dogBone.mirror(c.Y))
-dogBone.addLineGroup(dogBone.mirror(c.X))
-dogBone.translate(82.5, 9.5)
 
-halfWidth = 5.0    
-wideDogBone = s.Shape(None)
-wideDogBone.addLinesFromCoordinateList([[82.5, 0], [82.5, 9.5 + halfWidth], [49.642, 9.5 + halfWidth]])
-wideArc = a.Arc(p.Point(49.642, 9.5 + halfWidth), p.Point(28.5, 6.5 + halfWidth), c.CW, p.Point(28.5, 82.5 + halfWidth), 20)
-wideDogBone.addLineGroup(wideArc)
-wideDogBone.addLinesFromCoordinateList([[28.5, 6.5 + halfWidth], [0, 6.5 + halfWidth]])
-wideDogBone.addLineGroup(wideDogBone.mirror(c.Y))
-wideDogBone.addLineGroup(wideDogBone.mirror(c.X))
-wideDogBone.translate(82.5, 9.5 + halfWidth)
+shape = ds.wideDogBone
 
-#print dogBone
- 
-infill = InF.InFill(wideDogBone)
-fig = fg.Figura(infill)
+print '\nGenerating code, please wait...\n'
+#infill = InF.InFill()
+fig = fg.Figura(shape)
 
-#print infill
-
-#print 'dogBone'
-#print str(dogBone)
-#print 'Fig:'
-#print fig.gcode
-
-print '\nGenerating File, please wait...\n'
+print 'Code generated, writting file...\n'
 
 f = open(pr.outputSubDirectory+'\\'+pr.outputFileName, 'w')
 f.write(gc.startGcode())
