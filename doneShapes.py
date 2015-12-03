625729518f872e754bd195ff941d565aeaafe885
VanHulleOne <beardedone85@gmail.com>
2015-12-03 17:32:21 -0600

    Tested a shape with a hole.
    Seems to work.

--- doneShapes.py
+++ doneShapes.py
@@ -10,6 +10,10 @@ import arc as a
 import Point as p
 from parameters import constants as c
 
+aHole = a.Arc(p.Point(4,0), p.Point(4,0), c.CW, p.Point(0,0))
+hole = s.Shape(None)
+hole.addLineGroup(aHole)
+
 dogBone = s.Shape(None)
 dogBone.addLinesFromCoordinateList([[82.5, 0], [82.5, 9.5], [49.642, 9.5]])
 arc = a.Arc(p.Point(49.642, 9.5), p.Point(28.5, 6.5), c.CW, p.Point(28.5, 82.5), 20)
@@ -17,6 +21,7 @@ dogBone.addLineGroup(arc)
 dogBone.addLinesFromCoordinateList([[28.5, 6.5], [0, 6.5]])
 dogBone.addLineGroup(dogBone.mirror(c.Y))
 dogBone.addLineGroup(dogBone.mirror(c.X))
+dogBone.addInternalShape(hole)
 dogBone = dogBone.translate(82.5, 9.5)
 
 halfWidth = 5.0    
