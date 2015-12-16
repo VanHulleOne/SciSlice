3983f4db86f113de89f8f04f230ab73689c4b5e8
VanHulleOne <beardedone85@gmail.com>
2015-12-16 13:42:41 -0600

    Added new shapes
    Added separate left and right grips, a combined grips, and a center.

--- doneShapes.py
+++ doneShapes.py
@@ -36,5 +36,34 @@ class DoneShapes():
         wideDogBone.addLineGroup(wideDogBone.mirror(c.X))
         return wideDogBone.translate(82.5, 9.5 + halfWidth)
         
-
+    @property
+    def rightGrip(self):
+        shape = s.Shape(None)
+        shape.addLinesFromCoordinateList([[82.5, 0], [82.5, 9.5], [49.642, 9.5]])
+        arc = a.Arc(p.Point(49.642, 9.5), p.Point(28.5, 6.5), c.CW, p.Point(28.5, 82.5), 20)
+        shape.addLineGroup(arc)
+        shape.addLinesFromCoordinateList([[28.5, 6.5], [28.5, 0]])
+        shape.addLineGroup(shape.mirror(c.X))
+        return shape.translate(82.5, 9.5)
+        
+    @property
+    def leftGrip(self):
+        shape = s.Shape(self.rightGrip)
+        shape = shape.translate(-82.5, -9.5)
+        shape = shape.mirror(c.Y)
+        return shape.translate(82.5, 9.5)
+        
+    @property
+    def grips(self):
+        shape = self.leftGrip
+        shape.addLineGroup(self.rightGrip)
+        return shape
+    
+    @property
+    def center(self):
+        shape = s.Shape(None)
+        shape.addLinesFromCoordinateList([[28.5, 6.5], [-28.5, 6.5], [-28.5, -6.5],
+                                          [28.5, -6.5], [28.5, 6.5]])
+        return shape.translate(82.5, 9.5)
+        
 
