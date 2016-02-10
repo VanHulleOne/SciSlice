1af4070ca258b8d96116a4b8260cff9c185dd88e
VanHulleOne <vanhullel@my.uwstout.edu>
2016-02-10 12:16:59 -0600

    Can do shells easier
    Added a for loop to help create shells

--- Python1.py
+++ Python1.py
@@ -23,51 +23,17 @@ import time
 
 startTime = time.time()
 print '\nGenerating code, please wait...\n'
-    
-#    bigTest = False
-#    
-#    pattern = lg.LineGroup()
-#    pattern.addLinesFromCoordinateList([[0,0], [2,2], [4,0]])
-#    hexField = ds.hexField(1, pr.pathWidth/2, 180, 35)
-#    
-#    if bigTest:
-#        leftGrip = ds.leftGrip()
-#        center = ds.center()
-#        center.addLineGroup(ds.circle(82.5, 9.5, 4))
-#        rightGrip = ds.rightGrip()
-#        grips = ds.grips()
-#        grips2 = grips.offset(1, c.INSIDE)
-#        regDB = ds.regularDogBone()
-#        regDB1 = regDB.offset(0.5, c.OUTSIDE)
-#        regDB2 = regDB1.offset(0.5, c.OUTSIDE)
-#        sq1 = ds.squareWithHole()
-#        sq2 = sq1.offset(1, c.INSIDE)
-#        sq3 = sq2.offset(1, c.INSIDE)
-#        
-#        
-#        
-#        filledLeft = InF.InFill(leftGrip, pr.pathWidth, 45, pattern)
-#        filledCenter = InF.InFill(center, pr.pathWidth, 90, pattern)
-#        filledRight = InF.InFill(rightGrip, pr.pathWidth, -45, pattern)
-#        filledList = [filledLeft, filledCenter, filledRight, regDB1, regDB2]
-#        
-#    else:
-#        grips = ds.grips()
-#        leftGrip = ds.leftGrip()
-#        rightGrip = ds.rightGrip()
-#        center = ds.center()
-#        
-#        filledLeft = InF.InFill(leftGrip, pr.pathWidth, 90)
-#        filledRight = InF.InFill(rightGrip, pr.pathWidth, 90)
-#        filledGrips = InF.InFill(grips, pr.pathWidth, 90)
-#        filledCenter = InF.InFill(center, pr.pathWidth, 90)
-#        filledList = [filledGrips]
-    
-dbOutline = ds.regularDogBone()
-dbTrimShape = dbOutline.offset(0.5, c.INSIDE)
-dbInfill = InF.InFill(dbTrimShape, pr.pathWidth, pr.infillAngleDegrees)
+   
+currOutline = ds.regularDogBone()
+filledList = []
 
-filledList = [dbOutline, dbInfill]
+for shellNumber in range(pr.numShells):
+    filledList.append(currOutline)
+    currOutline = currOutline.offset(pr.pathWidth, c.INSIDE)
+
+infill = InF.InFill(currOutline, pr.pathWidth, pr.infillAngleDegrees)
+
+filledList.append(infill)
 
 
 print 'Created Infill: {:.2f}'.format(time.time()-startTime)
