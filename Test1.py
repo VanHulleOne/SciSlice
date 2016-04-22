c3ac270ef75cbb4b2c2651c94be204c25b1514a0
VanHulleOne <vanhullel@my.uwstout.edu>
2016-04-22 16:46:22 -0500

    Stuff

--- Test1.py
+++ Test1.py
@@ -35,10 +35,10 @@ CCW = 1
 #
 arc = a.Arc(p.Point(49.642, 9.5), p.Point(28.5, 6.5), CW, p.Point(28.5, 82.5), 20)
 #
-p1 = p.Point(-82.500, -9.500, 0)
-p2 = p.Point(74, -101.5052)
-p3 = p.Point(74, 101.5052)
-p4 = p.Point(-82.501, -9.5)
+p1 = p.Point(2.073, 0.0806)
+p2 = p.Point(2.1512, 0.0323)
+p3 = p.Point(2.144081, 0.0389)
+p4 = p.Point(2.0251, 0.1612)
 p5 = p.Point(3,3.0001)
 p6 = p.Point(0,0)
 p7 = p.Point(4,0)
@@ -46,3 +46,14 @@ p8 = p.Point(4,4)
 p9 = p.Point(0,4)
 p10 = p.Point(3,12)
 p11 = p.Point(0,5)
+
+
+for i in xrange(7):
+    if i%2:
+        st = 'top'
+        if i%3:
+            st = 'double'
+    else:
+        st = 'bottom'
+    print st
+        
