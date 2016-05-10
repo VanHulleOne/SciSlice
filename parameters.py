5a5deb6f6c036513666ec021287fa602d0bc4e4c
VanHulleOne <vanhullel@my.uwstout.edu>
2016-05-10 15:18:48 -0500

    Random Tests.

--- parameters.py
+++ parameters.py
@@ -10,13 +10,12 @@ import numpy
 """
 Printing Parameters.
 """
-gripSolidityRatio = 1.1 #fill for grips
 solidityRatio = [1.09]#12]#, 0.1, 0.05] solidityRatio = PathArea/beadArea
 pathWidth = 0.5 #mm distance between centerline of paths
 printSpeed = [2000] #mm/min head travel speed
-shiftX = [10]#, 50]#, 70]
+shiftX = [10, 50, 70]
 shiftY = [10]#, 35]
-firstLayerShiftZ = 0.1 #correct for bed leveling
+firstLayerShiftZ = 0 #correct for bed leveling
 
 """
 Part Parameters
@@ -24,7 +23,7 @@ Part Parameters
 #mainShape = ps.wideDogBone
 layerHeight = [0.4] #mm height per layer
 numLayers = [4] #number of layers to make
-infillAngleDegrees = [0, -45, 90, 45] #degrees infill angle 90 is in Y direction 0 is in X direction
+infillAngleDegrees = [0, -45, 90, 45, 45, 90, -45, 0] #degrees infill angle 90 is in Y direction 0 is in X direction
 numShells = 1 # the number of shells max is 13
 
 """
