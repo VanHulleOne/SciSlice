8b94191275740423c2deb4ca960994c3022c688c
VanHulleOne <vanhullel@my.uwstout.edu>
2016-05-16 12:41:52 -0500

    Added a comment for EPSILON

--- constants.py
+++ constants.py
@@ -16,5 +16,6 @@ CENTER = 3 #center of circle
 INSIDE = 1 #Point is inside shape
 OUTSIDE = 0 #point is outside shape
 ALLOW_PROJECTION = True
+EPSILON = 10000 # The prcision level to use when doing numerical comparisons
 OMIT_Z = True # When moving to a new point this chooses the Z level should not be used
 INCLUDE_Z = False
