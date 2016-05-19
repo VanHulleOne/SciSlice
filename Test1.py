8f7045143c22df5719bb89bb441b67b8b8fb18f4
VanHulleOne <vanhullel@my.uwstout.edu>
2016-05-19 14:22:01 -0500

    Testing

--- Test1.py
+++ Test1.py
@@ -28,30 +28,31 @@ import random
 import bisect
 import collections as col
 
-CW = -1
-CCW = 1
-
-#X, Y = 0, 1
+#CW = -1
+#CCW = 1
 #
-arc = a.Arc(p.Point(49.642, 9.5), p.Point(28.5, 6.5), CW, p.Point(28.5, 82.5), 20)
+##X, Y = 0, 1
+##
+#arc = a.Arc(p.Point(49.642, 9.5), p.Point(28.5, 6.5), CW, p.Point(28.5, 82.5), 20)
+##
+#p1 = p.Point(2.073, 0.0806)
+#p2 = p.Point(2.1512, 0.0323)
+#p3 = p.Point(2.144081, 0.0389)
+#p4 = p.Point(2.0251, 0.1612)
+#p5 = p.Point(3,3.0001)
+#p6 = p.Point(-1,0)
+#p7 = p.Point(4,0)
+#p8 = p.Point(4,4)
+#p9 = p.Point(0,4)
+#p10 = p.Point(3,12)
+#p11 = p.Point(0,5)
 #
-p1 = p.Point(2.073, 0.0806)
-p2 = p.Point(2.1512, 0.0323)
-p3 = p.Point(2.144081, 0.0389)
-p4 = p.Point(2.0251, 0.1612)
-p5 = p.Point(3,3.0001)
-p6 = p.Point(-1,0)
-p7 = p.Point(4,0)
-p8 = p.Point(4,4)
-p9 = p.Point(0,4)
-p10 = p.Point(3,12)
-p11 = p.Point(0,5)
+#l1 = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11]
+#
+#nv = [pi.normalVector for pi in l1]
 
-l1 = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11]
-
-nv = [pi.normalVector for pi in l1]
-
-    
+s1 = ds.regularDogBone()
+in1 = InF.InFill(s1, 0.5, 45)   
     
 """ An example of how to do other infills. """  
 #currOutline = ds.rect(0,0,15,250)
