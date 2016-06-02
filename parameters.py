9e431a994c1b3db03ea9995ad024daa8a7102dbb
VanHulleOne <vanhullel@my.uwstout.edu>
2016-06-02 18:23:37 -0500

    stuff

--- parameters.py
+++ parameters.py
@@ -25,8 +25,8 @@ Part Parameters
 outline = ds.regularDogBone() # The shape we will be printing
 solidityRatio = [1.09]#12]#, 0.1, 0.05] solidityRatio = PathArea/beadArea
 printSpeed = [2000] #mm/min head travel speed
-shiftX = [10, 50] # amount to shift part from printer origin in X
-shiftY = [10, 35, 60] # amount to shift part from printer origin in Y
+shiftX = [10]#, 50] # amount to shift part from printer origin in X
+shiftY = [10]#, 35, 60] # amount to shift part from printer origin in Y
 firstLayerShiftZ = 0 #correct for bed leveling
 numLayers = [2] #number of layers to make
 pattern = None
