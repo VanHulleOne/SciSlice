42c5f2f8a481e9d33cdee6ee10b09d368faebb97
die-bold <a.s.diebold@gmail.com>
2016-06-16 18:09:59 -0500

    just going to try to make the 3D model in a separate program
    Spent most of the day trying to get gui.py to work with showing the
    matplotlib graph. Tried many things, lots of googling, might have broken
    python on the computer, but nothing seemed to work. I've resigned to
    trying to set the program up to make the 3D model in a separate program
    and integrate it into gui.py at a later date.

--- gui.py
+++ gui.py
@@ -9,13 +9,13 @@ import matplotlib                   #for 3D model
 #if using the Spyder console, matplotlib is already imported and the backend cannot be changed with .use() as is needed
 #to change the backend to TkAgg, go to Tools > Preferences > Console > External modules > Matplotlib GUI backend
 #be sure to mind the caps in TkAgg
-
+"""
 backend = matplotlib.get_backend()
 if backend != "TkAgg" and backend != "module://ipykernel.pylab.backend_inline":
     matplotlib.use("TkAgg")             #backend of matplotlib, used for putting in GUI
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-
+"""
 from tkinter import *               #GUI module
 from tkinter import ttk             #for styling purposing
 from tkinter import filedialog      #window for saving and uploading files
@@ -377,18 +377,25 @@ class Page_Model(Frame):
         
         self.to_variables()
         
+        canvasExample = Canvas(self, width=200, height=100)
+        canvasExample.pack()      \
+        
+        canvasExample.create_rectangle(50, 20, 150, 80, fill="#476042")
+        canvasExample.create_line(0, 0, 50, 20, width=3)
+        
+        
         f = Figure(figsize=(5,5), dpi=100)
         a = f.add_subplot(111)
         a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
-        
+        """
         canvas = FigureCanvasTkAgg(f, self)
-        #canvas.show()
-        #canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
         
         toolbar = NavigationToolbar2TkAgg(canvas, self)
         toolbar.update()
-        #canvas._tkcanvas.pack()
-        
+        canvas._tkcanvas.pack()
+        """
         
         
     def to_variables(self):
---
+++ model_3D.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 17:57:12 2016
+
+@author: adiebold
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3],[5,7,4])
+
+plt.show()
