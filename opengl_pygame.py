86faf330d2d1c53f670127d5657378271d2dbb25
die-bold <a.s.diebold@gmail.com>
2016-07-08 17:05:02 -0500

    testing opengl and pygame, pygame installation directions
    Starting a program to test OpenGL and pygame. Made a folder for
    directions to install Python libraries. Added directions to install
    pygame.

--- opengl_pygame.py
+++ opengl_pygame.py
@@ -6,3 +6,35 @@ Created on Fri Jul  8 16:29:57 2016
 """
 
 #attempt at using opengl and pygame
+
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1),
+    )
+    
+edges = (
+    (0,1),    
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7),
+    )
