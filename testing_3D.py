92b201fbdca40d93c342841cd0f45b34e280917b
die-bold <a.s.diebold@gmail.com>
2016-08-23 15:43:42 -0500

    working on 3D model viewer
    Can now move to sides and zoom in/out

--- testing_3D.py
+++ testing_3D.py
@@ -58,6 +58,13 @@ class Wireframe:
             node.y = centre_y + scale * (node.y - centre_y)
             node.z *= scale
 
+key_to_function = {
+    pygame.K_LEFT:   (lambda x: x.translateAll('x', -10)),
+    pygame.K_RIGHT:  (lambda x: x.translateAll('x',  10)),
+    pygame.K_DOWN:   (lambda x: x.translateAll('y',  10)),
+    pygame.K_UP:     (lambda x: x.translateAll('y', -10)),
+    pygame.K_EQUALS: (lambda x: x.scaleAll(1.25)),
+    pygame.K_MINUS:  (lambda x: x.scaleAll( 0.8))}
             
 class ProjectionViewer:
     """ Displays 3D objects on a Pygame screen """
@@ -74,7 +81,7 @@ class ProjectionViewer:
         self.displayEdges = True
         self.nodeColour = (255,255,255)
         self.edgeColour = (200,200,200)
-        self.nodeRadius = 4
+        self.nodeRadius = 1
 
     def addWireframe(self, name, wireframe):
         """ Add a named wireframe object. """
@@ -90,33 +97,9 @@ class ProjectionViewer:
 #                    pygame.display.quit()
                     pygame.quit()
                     
-#                if event.type == pygame.KEYDOWN:
-#                    if event.key == pygame.K_LEFT:
-#                        glTranslatef(-10,0,0)
-#                    if event.key == pygame.K_RIGHT:
-#                        glTranslatef(10,0,0)
-#                    if event.key == pygame.K_UP:
-#                        glTranslatef(0,10,0)
-#                    if event.key == pygame.K_DOWN:
-#                        glTranslatef(0,-10,0)
-#                    if event.key == pygame.K_a:
-#                        glRotatef(10,0,1,0)
-#                    if event.key == pygame.K_d:
-#                        glRotatef(-10,0,1,0)
-#                    if event.key == pygame.K_w:
-#                        glRotatef(10,1,0,0)
-#                    if event.key == pygame.K_s:
-#                        glRotatef(-10,1,0,0)
-#                    if event.key == pygame.K_q:
-#                        glRotatef(10,0,0,1)
-#                    if event.key == pygame.K_e:
-#                        glRotatef(-10,0,0,1)
-#                        
-#                if event.type == pygame.MOUSEBUTTONDOWN:
-#                    if event.button == 4:
-#                        glTranslatef(0,0,10.0)
-#                    if event.button == 5:
-#                        glTranslatef(0,0,-10.0)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key in key_to_function:
+                        key_to_function[event.key](self)
                     
             self.display()  
             pygame.display.flip()
@@ -134,6 +117,21 @@ class ProjectionViewer:
             if self.displayNodes:
                 for node in wireframe.nodes:
                     pygame.draw.circle(self.screen, self.nodeColour, (int(node.x), int(node.y)), self.nodeRadius, 0)
+                    
+    def translateAll(self, axis, d):
+        """ Translate all wireframes along a given axis by d units. """
+
+        for wireframe in self.wireframes.values():
+            wireframe.translate(axis, d)
+
+    def scaleAll(self, scale):
+        """ Scale all wireframes by a given scale, centred on the centre of the screen. """
+
+        centre_x = self.width/2
+        centre_y = self.height/2
+
+        for wireframe in self.wireframes.values():
+            wireframe.scale(centre_x, centre_y, scale)
     
 if __name__ == '__main__':
     
@@ -147,23 +145,34 @@ if __name__ == '__main__':
             yar.append(point[1])
             zar.append(point[2])
             
-    print([(xar[c],yar[c],zar[c]) for c in range(len(xar))])
-    print([(n,n+1) for n in range(0,len(xar),2)])    
-    print('len nodes: ', len([(xar[c],yar[c],zar[c]) for c in range(len(xar))]))
-    print('len endges: ', len([(n,n+1) for n in range(0,len(xar),2)]))
-    print('len data: ', len(data))
-    print('len xar: ', len(xar))
+#    print([(xar[c],yar[c],zar[c]) for c in range(len(xar))])
+#    print([(n,n+1) for n in range(0,len(xar),2)])    
+#    print('len nodes: ', len([(xar[c],yar[c],zar[c]) for c in range(len(xar))]))
+#    print('len endges: ', len([(n,n+1) for n in range(0,len(xar),2)]))
+#    print('len data: ', len(data))
+#    print('len xar: ', len(xar))
             
 #    print(data)
+            
+    real = True
+    
+#    real = False
+    
     pv = ProjectionViewer(400, 300)
     cube = Wireframe()
-    cube.addNodes([(xar[c],yar[c],zar[c]) for c in range(len(xar))])
-    cube.addEdges([(n,n+1) for n in range(0,len(xar),2)])
+    
+    if real:
+        cube.addNodes([(xar[c],yar[c],zar[c]) for c in range(len(xar))])
+        cube.addEdges([(n,n+1) for n in range(0,len(xar),2)])
+    else:
+        cube.addNodes([(x,y,z) for x in (50,250) for y in (50,250) for z in (50,250)])
+        cube.addEdges([(n,n+4) for n in range(0,4)]+[(n,n+1) for n in range(0,8,2)]+[(n,n+2) for n in (0,1,4,5)])
+    
     pv.addWireframe('cube', cube)
     pv.run()
 
-#    cube.addNodes([(x,y,z) for x in (50,250) for y in (50,250) for z in (50,250)])
-#    cube.addEdges([(n,n+4) for n in range(0,4)]+[(n,n+1) for n in range(0,8,2)]+[(n,n+2) for n in (0,1,4,5)])
+    cube.addNodes([(x,y,z) for x in (50,250) for y in (50,250) for z in (50,250)])
+    cube.addEdges([(n,n+4) for n in range(0,4)]+[(n,n+1) for n in range(0,8,2)]+[(n,n+2) for n in (0,1,4,5)])
     
 #    print(len(data))
 #    print(len([(xar[c],yar[c],zar[c]) for c in range(len(xar))]))
