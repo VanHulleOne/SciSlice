d854dae5258cfe0430d9d66e075c9a5a9cf7f65c
lvanhulle <lvanhulle@wisc.edu>
2016-11-04 09:09:16 -0500

    This also needs a setup.py i guess.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'DogBoneV2',
+  packages = ['DogBoneV2'], # this must be the same as the name above
+  version = '2.2.2.2',
+  description = 'Generating Custom Tool Paths',
+  author = 'Luke Van Hulle',
+  author_email = 'lvanhulle@wisc.edu',
+  url = 'https://github.com/VanHulleOne/DogBoneV2', # use the URL to the github repo
+  download_url = 'https://github.com/VanHulleOne/DogBoneV2/archive/2.2.2.2.tar.gz', # I'll explain this in a second
+  keywords = ['3D Printing', 'Custom', 'Slicing', 'Tool', 'Path'], # keywords
+  classifiers = [],
+)
