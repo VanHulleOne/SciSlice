d74cdd05a26ffb6a21cc635b390841b2c321c369
lvanhulle <lvanhulle@wisc.edu>
2016-12-05 18:12:38 -0600

    add WaitRob so setDO

--- gcode.py
+++ gcode.py
@@ -84,8 +84,10 @@ class RobotCode:
         self.BLR = "DO6_Between_Layer_Retract"
         self.program_feed = "DO5_Program_Feed"
         
-    def setDO(name, value):
-        return '\t\tSetDO {}, {};\n'.format('name', value)
+    def setDO(self, name, value):
+        tempString = '\t\tWaitRob \InPos;\n'
+        tempString += '\t\tSetDO {}, {};\n'.format(name, value)
+        return tempString
     
     def feedMove(self, endPoint, omitZ, extrudeTo, printSpeed):
         #TODO: something for starting the extruder
@@ -139,9 +141,27 @@ class RobotCode:
         
     def comment(self, comment):
         return '\t\t' + self.pr.comment + ' ' + comment
-    
+        
     def startGcode(self,*, extruder_temp=0, bed_temp=0):
-        return 'MODULE MainModule\n\tPROC main()\n'
+        with open(self.pr.startEndSubDirectory + '\\' + self.pr.start_Gcode_FileName) as startFile:
+            lines = startFile.readlines()   
+        tempString = []
+        for line in lines:
+            line = str(line).replace(c.EXTRUDER_TEMP_FLAG, 'S'+str(extruder_temp))
+            line = line.replace(c.BED_TEMP_FLAG, 'S'+str(bed_temp))
+            tempString.append(line)
+        return ''.join(tempString)
     
     def endGcode(self):
-        return '\tENDPROC\nENDMODULE'
+        with open(self.pr.startEndSubDirectory + '\\' + self.pr.end_Gcode_FileName) as endFile:
+            lines = endFile.readlines()       
+        tempString = ''
+        for line in lines:
+            tempString += str(line)
+        return tempString
+    
+#    def startGcode(self,*, extruder_temp=0, bed_temp=0):
+#        return 'MODULE MainModule\n\tPROC main()\n'
+#    
+#    def endGcode(self):
+#        return '\tENDPROC\nENDMODULE'
