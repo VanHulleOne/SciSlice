b99eae919bb9a2b1c09440f1d7acdb9473ee7721
lvanhulle <lvanhulle@wisc.edu>
2016-12-06 13:05:40 -0600

    Clean up some start/endGcode stuff

--- gcode.py
+++ gcode.py
@@ -61,20 +61,16 @@ class Gcode:
     def startGcode(self,*, extruder_temp=0, bed_temp=0):
         with open(self.pr.startEndSubDirectory + '\\' + self.pr.start_Gcode_FileName) as startFile:
             lines = startFile.readlines()   
-        tempString = []
-        for line in lines:
-            line = str(line).replace(c.EXTRUDER_TEMP_FLAG, 'S'+str(extruder_temp))
-            line = line.replace(c.BED_TEMP_FLAG, 'S'+str(bed_temp))
-            tempString.append(line)
-        return ''.join(tempString)
+            tempString = []
+            for line in lines:
+                line = str(line).replace(c.EXTRUDER_TEMP_FLAG, 'S'+str(extruder_temp))
+                line = line.replace(c.BED_TEMP_FLAG, 'S'+str(bed_temp))
+                tempString.append(line)
+            return ''.join(tempString)
     
     def endGcode(self):
         with open(self.pr.startEndSubDirectory + '\\' + self.pr.end_Gcode_FileName) as endFile:
-            lines = endFile.readlines()       
-        tempString = ''
-        for line in lines:
-            tempString += str(line)
-        return tempString
+            return ''.join(endFile.readlines())
         
 class RobotCode:
     
@@ -150,12 +146,12 @@ class RobotCode:
     def startGcode(self,*, extruder_temp=0, bed_temp=0):
         with open(self.pr.startEndSubDirectory + '\\' + self.pr.start_Gcode_FileName) as startFile:
             lines = startFile.readlines()   
-        tempString = []
-        for line in lines:
-            line = str(line).replace(c.EXTRUDER_TEMP_FLAG, 'S'+str(extruder_temp))
-            line = line.replace(c.BED_TEMP_FLAG, 'S'+str(bed_temp))
-            tempString.append(line)
-        return ''.join(tempString)
+            tempString = []
+            for line in lines:
+                line = str(line).replace(c.EXTRUDER_TEMP_FLAG, 'S'+str(extruder_temp))
+                line = line.replace(c.BED_TEMP_FLAG, 'S'+str(bed_temp))
+                tempString.append(line)
+            return ''.join(tempString)
     
     def endGcode(self):
         with open(self.pr.startEndSubDirectory + '\\' + self.pr.end_Gcode_FileName) as endFile:
