181b753ee9bf9bf777cd9ef8671df0573626efbe
lvanhulle <lvanhulle@wisc.edu>
2017-01-27 16:35:17 -0600

    Remove old code

--- parameters.py
+++ parameters.py
@@ -123,44 +123,7 @@ class Parameters:
     
     def _updateAttributes(self, namedTuple):
         for key, value in namedTuple._asdict().items():
-                setattr(self, key, value)
-    
-    
-    
-
-#class Parameters2:
-#    
-#    def __init__(self, param_data, dropdown_data, layerParamLabels, partParamLabels):
-#        
-#        for key, value in param_data.items():
-#            setattr(self, key, value)
-#        
-#        self.currPath = os.path.dirname(os.path.realpath(__file__))
-#        
-#        self.startEndSubDirectory = self.currPath + '\\Start_End_Gcode'
-#        self.filamentArea = math.pi * self.filamentDiameter**2 / 4.0
-#
-#        #goes through each dictionary, check the 'the_label' dictionary entry
-#        #that has the dropdown.label as the value and using that value
-#        #along with setattr and getattr to set the appropriate variable
-#        #to the appropriate doneshape menu
-#        for data in dropdown_data: #x in range(len(dropdown_data)):
-#            the_label = data[c.THE_LABEL]
-#            if type(getattr(self, the_label)) == str:
-#                del data[c.THE_LABEL]
-#                if the_label == 'outline':
-#                    args = {key:value for key,value in data.items()}
-#                    setattr(self, 'outline_gen', getattr(ds, self.outline)(**args)) #lambda:getattr(ds, self.outline)(**args))
-#                else:
-#                    setattr(self, the_label, [getattr(ds, getattr(self, the_label))(**data)])
-#
-#        # to prevent having to type variable names three times we read in the fields from the namedtuple templates
-#        # and use them to generate the namedtuples
-#        layerParamLists = LayerParams(*[getattr(self, field) for field in LayerParams._fields]) 
-#
-#        self.layerParameters = lambda : zipVariables_gen(layerParamLists, repeat=True)
-#        
-#        self.everyPartsParameters = zipVariables_gen(PartParams(*[getattr(self, field) for field in PartParams._fields]))                     
+                setattr(self, key, value)               
   
 def zipVariables_gen(inputLists, repeat=False):
     if iter(inputLists) is iter(inputLists):
