f9a07c7b45eb4130b6367ae0f74f8094451a147b
lvanhulle <lvanhulle@wisc.edu>
2017-03-31 13:48:16 -0500

    Update version number for added randomStartLocation feature.

--- parameters.py
+++ parameters.py
@@ -6,7 +6,7 @@ Contains all of the print parameters
 @author: lvanhulle
 """
 """ Version Numbers: Main_Version.sub-version.Feature_Added.Bug_Fixed """
-__version__ = '0.3.1.0'
+__version__ = '0.3.2.0'
 
 from collections import namedtuple
 import itertools
