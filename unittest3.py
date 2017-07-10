5b667de4fa82ae54611b016ef0b67890cedf10f3
Siddhant Shah <siddhant0202@gmail.com>
2017-07-10 16:26:01 -0500

    Added tests for matrix transformations

--- unittest3.py
+++ unittest3.py
@@ -7,13 +7,13 @@ Created on Fri Jul  7 14:33:12 2017
 
 import unittest
 from point import Point
-from line import Line
-from linegroup import LineGroup
+#from line import Line
+#from linegroup import LineGroup
 from arc import Arc
 import matrixTrans as mt
 import constants as c
-import math
-import numpy as np
+#import math
+#import numpy as np
 #from shapely.geometry.polygon import Polygon
 #from shapely.ops import cascaded_union
 #from outline import Outline
@@ -26,14 +26,40 @@ class ArcTestCase(unittest.TestCase):
         ang = a.calcIncludedAngle(Point(1,1,0), Point(4,4,0))
         self.assertAlmostEqual(ang, 4.2426, 4)
         
-#    def test_ok(self):
-#        pass
-#class MatrixTestCase(unittest.TestCase):
-#    
-#    def test_matrix_translate(self):
-#        
-#        m = np.arange(1,17).reshape(4,4)
-#        print(m.t)
+class MatrixTestCase(unittest.TestCase):
+        
+    def test_matrix_mirror(self):
+        """ checks if the matrix is mirrored correctly about given axis """
+        self.assertTrue(tuple(mt.mirrorMatrix(c.X)[0]) == (1,0,0,0))
+        self.assertTrue(tuple(mt.mirrorMatrix(c.X)[1]) == (0,-1,0,0))
+        self.assertTrue(tuple(mt.mirrorMatrix(c.X)[2]) == (0,0,1,0))
+        self.assertTrue(tuple(mt.mirrorMatrix(c.X)[3]) == (0,0,0,1))
+        
+    def test_matrix_translate(self):
+        """ checks if the matrix is translated correctly """
+        self.assertTrue(tuple(mt.translateMatrix(2,3)[0]) == (1,0,0,2))
+        self.assertTrue(tuple(mt.translateMatrix(2,3)[1]) == (0,1,0,3))
+        self.assertTrue(tuple(mt.translateMatrix(2,3)[2]) == (0,0,1,0))
+        self.assertTrue(tuple(mt.translateMatrix(2,3)[3]) == (0,0,0,1))
+        
+    def test_matrix_rotate(self):
+        """ checks if the matrix is rotated correctly """
+        self.assertTrue(tuple(mt.rotateMatrix(0)[0]) == (1,0,0,0))
+        self.assertTrue(tuple(mt.rotateMatrix(0)[1]) == (0,1,0,0))
+        self.assertTrue(tuple(mt.rotateMatrix(0)[2]) == (0,0,1,0))
+        self.assertTrue(tuple(mt.rotateMatrix(0)[3]) == (0,0,0,1))
+ 
+    def test_matrix_scale(self):
+        """ checks if the matrix is scaled accordingly """
+        self.assertTrue(tuple(mt.scale(5,4)[0]) == (5,0,0,0))
+        self.assertTrue(tuple(mt.scale(5,4)[1]) == (0,4,0,0))
+        self.assertTrue(tuple(mt.scale(5,4)[2]) == (0,0,1,0))
+        self.assertTrue(tuple(mt.scale(5,4)[3]) == (0,0,0,1))
+        
+    def test_matrix_combine_transformations(self):
+        """ checks if dot product with matrix list is correct """
+        ml = [[1,2,3,4],[5,6,7,8]]
+        self.assertTrue(mt.combineTransformations(ml) == 70)
         
 def main():
     unittest.main()
